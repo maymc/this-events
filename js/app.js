25e9355d083e20e576eea3f75ad71e5d3b283038
May Chen <maychen@Mays-MacBook-Pro.local>
2018-02-26 21:33:20 -1000

    completed ex1

--- js/app.js
+++ js/app.js
@@ -4,11 +4,28 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var navMenuElem = document.getElementsByClassName("navi");
 
+for(var i=0; i<navMenuElem.length; i++){
+	navMenuElem[i].addEventListener('click', showHide);
+}
 
+function showHide(){
+	var aElem= this.querySelectorAll('.inner')[0];
+
+	if(aElem.style.display==="none"){
+		aElem.style.display = 'block';
+  	}
+	
+	else{
+		aElem.style.display='none';
+	}
+}
 
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+
+
 
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
