13376afa99bf8fdbbda0ef9a2b06a7747a75a923
May Chen <maychen@Mays-MacBook-Pro.local>
2018-02-27 23:31:13 -1000

    attempt 1 ex3

--- js/app.js
+++ js/app.js
@@ -43,23 +43,61 @@ function showHideMenu(){
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
 
-var descripElem = document.getElementsByClassName("descrip");
+var rankElem = document.getElementsByClassName("rank");
 
-for(var k=0; k<descripElem.length; k++){
-	descripElem[k].addEventListener('click', clickThumb);
-}
-
-function clickThumb(){
-	var dElem = this.querySelectorAll('.thumb')[0];
-	dElem.counter
+for(var k=0; k<rankElem.length; k++){
+	rankElem[k].addEventListener('click', countClicks);
 }
 
 
 
+var numClicks= 0;
+var count = document.createElement('p');
+
+function countClicks(){
+	var thumbElem = this.querySelectorAll('.thumb')[0];
+
+	numClicks += 1;
+	console.log(numClicks);
+
+	count.id = 'count';
+	count.innerHTML = numClicks;
+	var thumbParent = document.getElementsByClassName('thumb');
+	thumbParent[0].appendChild(count);
+}
 
 
 
 
 
+/*var numClicks1= 0;
+var numClicks2 = 0;
+var numClicks3 = 0;
+var count1 = document.createElement('p');
+var count2 = document.createElement('p');
+var count3 = document.createElement('p');
+
+function countClicks(){
+	var thumbElem = this.querySelectorAll('.thumb')[0];
+
+	numClicks1 += 1;
+	numClicks2 += 1;
+	numClicks3 += 1;
+
+	count1.id = 'count1';
+	count1.innerHTML = numClicks1;
+	var thumbParent1 = document.getElementsByClassName('thumb');
+	thumbParent1[0].appendChild(count1);	
+
+	count2.id = 'count1';
+	count2.innerHTML = numClicks2;
+	var thumbParent2 = document.getElementsByClassName('thumb');
+	thumbParent2[1].appendChild(count2);	
+
+	count3.id = 'count1';
+	count3.innerHTML = numClicks3;
+	var thumbParent3 = document.getElementsByClassName('thumb');
+	thumbParent3[2].appendChild(count3);
 
 
+}*/
